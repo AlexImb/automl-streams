4fd0fc318ed9fb1af2f8de2af5506b57e8021af2
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2020-01-13 14:49:26 +0100

    Add online metastream comparison

---
+++ demos/kafka_stream_meta_classification_comp.py
@@ -0,0 +1,30 @@
+from automlstreams.streams import KafkaStream
+from skmultiflow.evaluation import EvaluatePrequential
+from automlstreams.meta import MetaClassifier
+from skmultiflow.meta import LeverageBagging
+
+DEFAULT_INPUT_TOPIC = 'sea_big'
+DEFAULT_BROKER = 'localhost:9092'
+
+
+def run(topic=DEFAULT_INPUT_TOPIC, broker=DEFAULT_BROKER):
+    print(f'Running demo for topic={topic} and broker={broker}')
+    stream = KafkaStream(topic, bootstrap_servers=broker)
+    stream.prepare_for_use()
+
+    classifiers = [
+        MetaClassifier(),
+        LeverageBagging()
+    ]
+
+    evaluator = EvaluatePrequential(show_plot=True,
+                                    n_wait=200,
+                                    batch_size=50,
+                                    pretrain_size=500,
+                                    max_samples=5000)
+
+    evaluator.evaluate(stream=stream, model=classifiers)
+
+
+if __name__ == "__main__":
+    run()
