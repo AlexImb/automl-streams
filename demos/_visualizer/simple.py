7f35d2e726294074a51c87e30789eb0b32ef26fb
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2020-01-18 01:13:50 +0100

    Add visualizer for demo results

--- demos/_visualizer/simple.py
+++ demos/_visualizer/simple.py
@@ -3,20 +3,26 @@ import pandas as pd
 import matplotlib.style as style
 style.use('seaborn-poster')
 
+def plot_topics(demo, demo_type, topics):
+    Y = pd.DataFrame()
 
-def plot(path):
-    df = pd.read_csv(path, comment='#')
-    print(df)
-    print(df.columns)
-    x = df[['id']]
-    y = df.drop(columns=['id'])
-    plt.plot(x, y)
+    for topic in topics:
+        path = get_path(demo, demo_type, topic)
+        df = pd.read_csv(path, comment='#')
+        x = df[['id']]
+        y = df[['mean_acc_[M0]']]
+        Y = pd.concat([Y, y], axis=1)
+
+    plt.plot(x, Y)
+    plt.legend(topics)
     plt.show()
 
 
+def get_path(demo, demo_type, topic):
+    return f'{demo}/results/{demo_type}_{topic}.csv'
+
+
 if __name__ == "__main__":
-    demo = 'auto-sklearn'
-    demo_type = 'batch'
-    topic = 'sea_big'
-    path = f'{demo}/results/{demo_type}_{topic}.csv'
-    plot(path)
+    topics = ['elec', 'covtype', 'weather', 'sea_big', 'moving_squares']
+    plot_topics('auto-sklearn', 'batch', topics)
+
